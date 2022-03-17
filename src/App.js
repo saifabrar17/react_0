de0ad71cd12811cd5fd61c6b3ec609c6647b1af4
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-17 11:47:16 +0600

    create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  {name:'Dr. Mahfuz', job:'Singer'},
+  {name:'Tailor Swift', job:'Actor'},
+  {name:'Dj Snake', job:'DJ'},
+  {name:'Pitbull', job:'Singer'}
+]
 function App() {
+  const nayoks = ['Kopila', 'BappaRaz','Ananta', 'Shakib Khan']
   return (
     <div className="App">
-      <Person name="Rubel"></Person>
-      <Person name="bappaRaz"></Person>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li> )
+      }
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+      <h4>======</h4>
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+
+
+
+      <h4>======</h4>
+      <Person name={nayoks[0]}></Person>
+      <Person name={nayoks[1]}></Person>
       <h4>New Component</h4>
-      <Friend></Friend>
-      <Friend></Friend>
+      {/* <Friend></Friend>
+      <Friend></Friend> */}
     </div>
   );
 } 
@@ -18,7 +39,7 @@ function Person(props) {
   return (
     <div className="jhanker">
       <h1>{props.name}</h1>
-      <p>Profession: Programmer</p>
+      {/* <p>Profession: Programmer</p> */}
     </div>
   )
 }
