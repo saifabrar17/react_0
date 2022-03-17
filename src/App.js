502a98e19c156315bd8b9c87d6335b406ca7cf5e
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-17 11:16:54 +0600

    pass dynamic data to components, props

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Rubel"></Person>
+      <Person name="bappaRaz"></Person>
+      <h4>New Component</h4>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
-}
+} 
 
+function Person(props) {
+  console.log(props);
+  return (
+    <div className="jhanker">
+      <h1>{props.name}</h1>
+      <p>Profession: Programmer</p>
+    </div>
+  )
+}
+function Friend() {
+  return (
+    <div className="jalil">
+      <h2>Name: Ananta Jalil</h2>
+      <p>Profession: Actor</p>
+    </div>
+  )
+}
 export default App;
