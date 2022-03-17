39c61774aa2368e1f3122a9a8066e058b303bb22
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-17 22:27:21 +0600

    final module video practise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
+    </div>
+  )
+}
+
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]); // use state er moddhe [] default value hisebe kaj korbe 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  //age ekta comma diye empty array declare kore dewa uchit, emt arr te dependancy nai
+  //tarpor empty arrow function declare korlam
+  return (
+    <div>
+      <h2>External Users</h2>
+      <h4>{users.length}</h4>
+      {
+        users.map(user => <Users name={user.name} email={user.email}></Users> )
+      }
+    </div>
+  )
+}
+
+function Users(props){
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
     </div>
   )
 }
